51a4281cd0764d7444c4495de340e5b139af23db
shabab-haider <shababh436@gmail.com>
2026-03-27 10:58:13 +0500

    Add missing context file

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RouterProvider } from "react-router";
 import { Router } from "./app.routes";
 import { AuthProvider } from "./features/auth/auth.context.jsx";
-import InterviewContext from "./features/interview/interview.context.jsx";
+import InterviewContext from "./features/interview/Interview.context.jsx";
 import AppNavbar from "./components/AppNavbar.jsx";
 
 const App = () => {
