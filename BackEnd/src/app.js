ea3c4a7d0ccbd63e7947c2d57a03e09d3a1e5c93
shabab-haider <shababh436@gmail.com>
2026-03-27 11:16:15 +0500

    Cors Fixed according to vercel Link ✔

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://resume-gen-ai-kocz.vercel.app", credentials: true }));
 
 /** Require all routes here */
 const authRoutes = require("../src/routes/auth.routes");
