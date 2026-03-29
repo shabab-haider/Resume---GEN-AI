606018d3886088baabb8a606b612111817e9f327
shabab-haider <shababh436@gmail.com>
2026-03-29 08:59:22 +0500

    port variable setup

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,8 @@ const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
 connectToDB();
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-  console.log(`App is runing on port 3000`);
+app.listen(port, () => {
+  console.log(`App is runing on port ${port}`);
 });
